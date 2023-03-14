4618215c086e60d36d77de0b20bb1ea5a2f69087
unknown <paschenkomih@gmail.com>
2023-03-15 01:42:21 +0200

    Added Task4. Operations with two strings

---
+++ Lesson_1/Task4/Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Entire first string: ");
+string string1 = Console.ReadLine();
+
+Console.Write("Entire second string: ");
+string string2 = Console.ReadLine();
+
+if (string1.Length > string2.Length)
+{
+    Console.WriteLine($"\nResult: {string1} {string2}");
+}
+else if (string1.Length == string2.Length)
+{
+    Console.WriteLine(" ");
+}
+else
+{
+    try
+    {
+        string[] splitString = string2.Split(string1[0]);
+        Console.WriteLine("\nResult: ");
+        foreach (string i in splitString) Console.WriteLine($"{i} ");
+    }
+    catch
+    {
+        Console.WriteLine("\nError, can`t split longest string on first symbol shortest string");
+    }
+}
