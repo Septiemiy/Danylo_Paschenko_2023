75263e0751db214f897b42006c6ebc1ba5eceb7c
unknown <pachenkomih@gmail.com>
2023-03-27 00:02:11 +0300

    Added Lesson 1 Task 3. Duplicate number in list which is entered by the user

--- Lesson_1/Task3/Task3/Program.cs
+++ Lesson_1/Task3/Task3/Program.cs
@@ -1,5 +1,4 @@
 ﻿List<string> numbers = new List<string>();
-List<int> numberDuplicationIndex= new List<int>();
 
 Console.WriteLine("Entire 10 numbers: ");
 for (int i = 0; i < 10; i++)
@@ -7,35 +6,17 @@ for (int i = 0; i < 10; i++)
     numbers.Add(Console.ReadLine());
 }
 
-Console.WriteLine("\nTo exit the program, entire \"q\"\n");
-while (true)
+Console.Write("Entire number to dublicate: ");
+string userSymbol = Console.ReadLine();
+
+if (numbers.Contains(userSymbol))
 {
-    numberDuplicationIndex.Clear();
-
-    string userSymbol = Console.ReadLine();
-
-    if (userSymbol == "q") break;
-
-    if (numbers.Contains(userSymbol))
-    {
-        for (int i = 0; i < numbers.Count; i++)
-        {
-            if (numbers[i] == userSymbol) numberDuplicationIndex.Add(i);
-        }
-    }
-    else
-    {
-        Console.WriteLine("\nList of numbers don`t have such number or entered symbol isn't a number\n");
-        continue;
-    }
-
-    for (int i = 0; i < numberDuplicationIndex.Count; i++)
-    {
-        Console.WriteLine("+");
-        numbers.Insert(numberDuplicationIndex[i], userSymbol);
-    }
-    foreach (int i in numberDuplicationIndex) Console.Write($"{i} ");
-    Console.Write("Result: ");
-    foreach (string i in numbers) Console.Write($"{i} ");
-    Console.WriteLine("\n");
+    numbers.Insert(numbers.IndexOf(userSymbol, 0), userSymbol);
 }
+else
+{
+    Console.WriteLine("\nList of numbers don`t have such number or entered symbol isn't a number\n");
+}
+
+Console.Write("Result: ");
+foreach (string i in numbers) Console.Write($"{i} ");
