1459eacd5c97cb2db0259b211afd8266cc081f64
unknown <pachenkomih@gmail.com>
2023-04-16 22:38:11 +0300

    Added Lesson 4 Task 1. Single Responsibility

---
+++ Lesson_4/Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SingleResponsibility
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+
+        public Customer(int id, string name, decimal balance)
+        {
+            Id = id;
+            Name = name;
+            Balance = balance;
+        }
+
+        public List<Customer> CustomersList = new List<Customer>()
+        {
+            new Customer(1, "Fikus", 0),
+            new Customer(2, "VHarbar", 100000)
+        };
+    }
+
+    public class GetCustomerBalance
+    {
+        public Customer Customer { get; }
+
+        public void GetBalance(int id)
+        {
+            Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
+        }
+        public decimal GetBalanceById(int id)
+        {
+            var customer = Customer.CustomersList.FirstOrDefault(x => x.Id == id);
+            return customer.Balance;
+        }
+    }
+
+    public class UpdateBalanceAndSaveDB
+    {
+        public Customer Customer { get; }
+        public Customer GetById(int id)
+        {
+            return Customer.CustomersList.FirstOrDefault(x => x.Id == id);
+        }
+        public void SaveToDatabase()
+        {
+            Console.WriteLine("Saved!");
+        }
+        public void UpdateBalance(int id, decimal newBalance)
+        {
+            var customer = GetById(id);
+            customer.Balance = newBalance;
+            SaveToDatabase();
+        }
+    }
+}
