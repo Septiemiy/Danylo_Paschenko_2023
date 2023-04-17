b476ce29374407ad01d6824cf876d28f5fe1cda3
unknown <pachenkomih@gmail.com>
2023-04-17 03:36:27 +0300

    Split program to separate files

---
+++ Lesson_4/Task1/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace SingleResponsibility
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+
+        public Customer(int id, string name, decimal balance)
+        {
+            Id = id;
+            Name = name;
+            Balance = balance;
+        }
+
+        public List<Customer> CustomersList = new List<Customer>()
+        {
+            new Customer(1, "Fikus", 0),
+            new Customer(2, "VHarbar", 100000)
+        };
+    }
+}
---
+++ Lesson_4/Task1/GetCustomerBalance.cs
@@ -0,0 +1,24 @@
+﻿using SingleResponsibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class GetCustomerBalance
+    {
+        public Customer Customer { get; }
+
+        public void GetBalance(int id)
+        {
+            Console.WriteLine($"Your balance is: {GetBalanceById(id)}");
+        }
+        public decimal GetBalanceById(int id)
+        {
+            var customer = Customer.CustomersList.FirstOrDefault(x => x.Id == id);
+            return customer.Balance;
+        }
+    }
+}
