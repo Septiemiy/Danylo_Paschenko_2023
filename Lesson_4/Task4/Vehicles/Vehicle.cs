b83f9df239b699e71fc046692ebdb9ec4efa6604
unknown <pachenkomih@gmail.com>
2023-04-19 16:48:07 +0300

    Delete not needed property in class Vehicle and delete modificator public in interface class

--- Lesson_4/Task4/Vehicles/Vehicle.cs
+++ Lesson_4/Task4/Vehicles/Vehicle.cs
@@ -8,9 +8,6 @@ namespace LiskovSubstitution.Vehicles
 {
     public class Vehicle
     {
-        public string Car { get; }
-        public string Motorcycle { get; }
-        public string Airplane { get; }
         public virtual void StartEngine()
         {
             Console.WriteLine("The engine is starting");
