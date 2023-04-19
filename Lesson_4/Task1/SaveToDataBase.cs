d3a110ff55c5eca925c88df8e8f4e917c3f4ccd4
unknown <pachenkomih@gmail.com>
2023-04-19 16:10:08 +0300

    Transferred SvingToDataBase to a separate class

---
+++ Lesson_4/Task1/SaveToDataBase.cs
@@ -0,0 +1,17 @@
+﻿using SingleResponsibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class SaveToDataBase
+    {
+        public void SaveToDatabase()
+        {
+            Console.WriteLine("Saved!");
+        }
+    }
+}
---
+++ Lesson_4/Task1/UpdateCustomerBalance.cs
@@ -0,0 +1,24 @@
+﻿using SingleResponsibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class UpdateCustomerBalance : SaveToDataBase
+    {
+        public Customer Customer { get; }
+        public Customer GetById(int id)
+        {
+            return Customer.CustomersList.FirstOrDefault(x => x.Id == id);
+        }
+        public void UpdateBalance(int id, decimal newBalance)
+        {
+            var customer = GetById(id);
+            customer.Balance = newBalance;
+            SaveToDatabase();
+        }
+    }
+}
