1c4b49d125678450e656af59856a9b138ba3e6e2
unknown <pachenkomih@gmail.com>
2023-04-22 01:34:43 +0300

    Transfered interfaces to separate files

---
+++ Lesson_4/Task3/Interfaces/IAdministrator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public interface IAdministrator
+    {
+        void WriteToFile(string filename);
+        void SaveToFile(string filename);
+        void GetDataFromFile(string filename);
+    }
+}
---
+++ Lesson_4/Task3/Interfaces/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public interface IUser
+    {
+        string ReadFromFile(string filename);
+        void DownloadFile(string filename);
+        void CopyFile(string filename);
+        void CheckFile(string filename);
+    }
+}
